08a61edaa776ae51a4f63804fe8e87725aafc632
Bogdanc2004 <bogdan.comsa.2004@gmail.com>
2025-04-17 18:34:45 +0300

    Week8

---
+++ treasure_hub.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    if(!strcmp("--start_monitor", argv[1])){
+
+    }
+    else if(!strcmp("--list_hunts", argv[1])){
+
+    }
+    else if(!strcmp("--list_treasure", argv[1])){
+
+    }
+    else if(!strcmp("--stop_monitor", argv[1])){
+
+    }
+    else if(!strcmp("--exit", argv[1])){
+        
+    }
+    return 0;
+}
